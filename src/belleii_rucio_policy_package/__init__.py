d7146624f5380c8b9e3443f587f729e2c8bb4bd0
rdimaio <riccardodimaio11@gmail.com>
2024-11-07 15:05:27 +0100

    Fix imports

--- src/belleii_rucio_policy_package/__init__.py
+++ src/belleii_rucio_policy_package/__init__.py
@@ -1,18 +1,18 @@
-import non_deterministic_pfn
-import scope
-import lfn2pfn
+from belleii_rucio_policy_package.non_deterministic_pfn import BelleIINonDeterministicPFNAlgorithm
+from belleii_rucio_policy_package.scope import BelleIIScopeExtractionAlgorithm
+from belleii_rucio_policy_package.lfn2pfn import BelleIIRSEDeterministicTranslation
 
 SUPPORTED_VERSION = ["35"]  # Only use with Rucio >=35.1.0 - pending https://github.com/rucio/rucio/issues/7082
 
 def get_algorithms():
     return { 
         'non_deterministic_pfn': {
-            'belleii_non_deterministic_pfn': non_deterministic_pfn.BelleIINonDeterministicPFNAlgorithm.construct_non_deterministic_pfn_belleii
+            'belleii_non_deterministic_pfn': BelleIINonDeterministicPFNAlgorithm.construct_non_deterministic_pfn_belleii
             },
          'lfn2pfn': {
-             'belleii_lfn2pfn': lfn2pfn.BelleIIRSEDeterministicTranslation.lfn2pfn_belleii
+             'belleii_lfn2pfn': BelleIIRSEDeterministicTranslation.lfn2pfn_belleii
              },
          'scope': { 
-             'belleii_extract_scope': scope.BelleIIScopeExtractionAlgorithm.extract_scope_belleii
+             'belleii_extract_scope': BelleIIScopeExtractionAlgorithm.extract_scope_belleii
              } 
     }
